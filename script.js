47531c16631bcad448ecf3a6a1c0da1a2cfb3f8c
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-03 12:08:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-let result = '';
+const romanNumerals = [
+    ['M', 1000],
+    ['D', 500],
+    ['C', 100],
+    ['L', 50],
+    ['X', 10],
+    ['V', 5],
+    ['I', 1]
+];
 
-    for (let i = 0; i < Object.keys(obj).length; i++) {
-        while (num >= obj[i][1]) {
-            result += obj[i][0];
-            num -= obj[i][1];
+function decimalToRoman(num) {
+    let result = '';
+    // Iterate through the romanNumerals array
+    for (let i = 0; i < romanNumerals.length; i++) {
+        const [roman, value] = romanNumerals[i];
+        // Check how many times the current value fits into the given number
+        while (num >= value) {
+            result += roman; // Append the corresponding Roman numeral
+            num -= value;    // Subtract the value from the given number
         }
     }
-
     return result;
 }
+
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
+// console.log(convertToRoman(36));
 
 
 
